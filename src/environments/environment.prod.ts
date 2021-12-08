c784ede3519184a68da388baf69062f3732bca3e
santoshtamhane <santosh.tamhane@foretelics.com>
2021-12-08 12:25:19 +0530

    Add Prod Environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+    firebase: {
+    apiKey: 'AIzaSyDrLR3C3xfwuRoklS7WZVsx6VFMWkl4cS0',
+    authDomain: 'dailysaletrack.firebaseapp.com',
+    databaseURL: 'https://dailysaletrack.firebaseio.com',
+    projectId: 'dailysaletrack',
+    storageBucket: 'dailysaletrack.appspot.com',
+    messagingSenderId: '752988861204',
+    appId: '1:752988861204:web:266e3599c0a87a75c0c801',
+    measurementId: 'G-6XRMVJ5B0V'
+  }
 };
